61b6b44b817c7c81b043f65a9aac3b3445252db9
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-17 22:21:53 +0530

    .

---
+++ GreenSock/EP1/script.js
@@ -0,0 +1,61 @@
+// gsap.to('#box', {
+//     x: "900px",
+//     rotate: "360deg",
+//     scale: "0.5",
+//     backgroundColor: "blue",
+//     duration: "10",
+//     delay: "2"
+// });
+
+gsap.from('#box', {
+    x: "900px",
+    rotate: "360deg",
+    scale: "0.5",
+    backgroundColor: "blue",
+    duration: "6",
+    delay: "2"
+});
+
+gsap.to('h1', {
+    x: '900px',
+    color: "orange",
+    duration: '3',
+    delay: '2'
+});
+gsap.to('h2', {
+    x: '900px',
+    color: "salmon",
+    duration: '3',
+    delay: '4'
+});
+gsap.to('h3', {
+    x: '900px',
+    color: "purple",
+    duration: '3',
+    delay: '6'
+});
+
+gsap.to('p', {
+    x: '900px',
+    color: "purple",
+    duration: '3',
+    delay: '6',
+    stagger:'1' // makes all elements inside <p></p> tags animate one by one with time interval of 1 second between them
+});
+
+gsap.to('h6', {
+    x: '700px',
+    color: "purple",
+    duration: '3',
+    delay: '2',
+    repeat:'-1'
+});
+
+gsap.to('h5', {
+    x: '700px',
+    color: "purple",
+    duration: '3',
+    delay: '2',
+    repeat:'-1',
+    yoyo: true
+});
