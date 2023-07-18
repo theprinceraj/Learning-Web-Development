da42815998dfe1a4233ff3129c6cef53eb6859ec
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-18 20:53:28 +0530

    .

--- GreenSock/EP4/script.js
+++ GreenSock/EP4/script.js
@@ -6,14 +6,14 @@ tl.from('#nav', {
 });
 
 tl.from('#nav h1, #nav h3, #nav h4, #nav i', {
-    y:-80,
+    y: -80,
     opacity: 0,
     duration: 1,
     stagger: 0.2,
 });
 
 tl.from('#left h1', {
-    x:-500,
+    x: -500,
     opacity: 0,
     duration: 1,
     stagger: 0.2
@@ -24,4 +24,28 @@ tl.from('#right', {
     scale: 2,
     duration: 0.5,
 
+});
+
+
+tl.from('#left-bottom', {
+    opacity: 0,
+});
+tl.from('#left-bottom h3', {
+    opacity: 0,
+    x: -500,
+    opacity: 0,
+    stagger: 0.2
+});
+
+
+gsap.from('#page2 .box', {
+    opacity: 0,
+    x: -100,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: '#page2 .box',
+        scroller: 'body',
+        end: 'top 30%',
+        scrub: 2,
+    }
 });
