787d3ec2049d33d555a3c8fe17ea8d0a6420b781
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-18 20:08:58 +0530

    gsap

--- GreenSock/EP4/script.js
+++ GreenSock/EP4/script.js
@@ -1,9 +1,27 @@
 let tl = gsap.timeline();
 
+tl.from('#nav', {
+    opacity: 0,
+    delay: 0.3,
+});
+
 tl.from('#nav h1, #nav h3, #nav h4, #nav i', {
     y:-80,
     opacity: 0,
     duration: 1,
-    delay: 0.7,
     stagger: 0.2,
+});
+
+tl.from('#left h1', {
+    x:-500,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2
+});
+
+tl.from('#right', {
+    opacity: 0,
+    scale: 2,
+    duration: 0.5,
+
 });
